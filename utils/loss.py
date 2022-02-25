d47a46cbc4631734644f7150b87e7d4062ebea7a
shehanmunasinghe <shehanmunasinghe@gmail.com>
2022-02-25 17:21:56 +0530

    new loss - DifferenceDailyReportedAndDead

--- utils/loss.py
+++ utils/loss.py
@@ -45,10 +45,47 @@ class DifferenceDailyReported:
 
         return loss
 
+class DifferenceDailyReportedAndDead:
+    '''  Calculate loss based on difference in daily reported cases'''
+
+    def __init__(self, population_denormalization_factor = 1) -> None:
+        self.population_denormalization_factor = population_denormalization_factor
+        
+
+    def __call__(self, pred_y, gt_y):
+        '''
+        Args: 
+            pred_y: (num_days, 4)
+            gt_y: (num_days, 4)
+        '''
+        # Total reported 
+        total_reported_pred = pred_y[:,1]+pred_y[:,2]+pred_y[:,3]
+        total_reported_gt   = gt_y[:,1]+gt_y[:,2]+gt_y[:,3]
+
+        # forward difference (daily change) of reported
+        daily_reported_pred = total_reported_pred[1:] - total_reported_pred[:-1]
+        daily_reported_gt   = total_reported_gt[1:] - total_reported_gt[:-1]
+
+        # Total deaths
+        total_dead_pred = pred_y[:,3]
+        total_dead_gt   = gt_y[:,3]
+        # forward difference (daily change) of deaths
+        daily_dead_pred = total_dead_pred[1:] - total_dead_pred[:-1]
+        daily_dead_gt = total_dead_gt[1:] - total_dead_gt[:-1]
+
+        loss_reported = nn.functional.smooth_l1_loss(daily_reported_pred,daily_reported_gt) * self.population_denormalization_factor #/1000 * self.population
+        loss_dead = nn.functional.smooth_l1_loss(daily_dead_pred,daily_dead_gt) * self.population_denormalization_factor #/1000 * self.population
+        # loss_reported = nn.functional.mse_loss(daily_reported_pred,daily_reported_gt) * self.population_denormalization_factor #/1000 * self.population
+        # loss_dead = nn.functional.mse_loss(daily_dead_pred,daily_dead_gt) * self.population_denormalization_factor #/1000 * self.population
+
+        return loss_reported + loss_dead
+
 def build_loss_from_config(conf, population_denormalization_factor=1):
     if conf["OPTIMIZATION"]["CRITERION"] == 'SmoothL1Loss':
         return torch.nn.SmoothL1Loss()
     elif conf["OPTIMIZATION"]["CRITERION"] == 'DifferenceDailyReported':
         return DifferenceDailyReported(population_denormalization_factor)
+    elif conf["OPTIMIZATION"]["CRITERION"] == 'DifferenceDailyReportedAndDead':
+        return DifferenceDailyReportedAndDead(population_denormalization_factor)
         
     return None
